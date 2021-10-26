9e515bbd9bbf1b4b7855aa59cf4407cd2bdbf2db
5oN1A <guarino.sonia@gmail.com>
2021-10-26 15:46:27 +0200

    milestone 1

--- js/script.js
+++ js/script.js
@@ -1,107 +1,11 @@
-let iconsList = [
-	{
-		name: 'cat',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'crow',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'dog',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'dove',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'dragon',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'horse',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'hippo',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'fish',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'carrot',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas'
-	},
-	{
-		name: 'apple-alt',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas'
-	},
-	{
-		name: 'lemon',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas'
-	},
-	{
-		name: 'pepper-hot',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas'
-	},
-	{
-		name: 'user-astronaut',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas'
-	},
-	{
-		name: 'user-graduate',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas'
-	},
-	{
-		name: 'user-ninja',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas'
-	},
-	{
-		name: 'user-secret',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas'
-	}
-];
+
 
 /*Milestone 1
 Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
 */
 
-//1. creare un ciclo in cui per ogni oggetto dell'array inserisco la proprietà colore (che dipende dal type)
+//1.1 creare un ciclo in cui per ogni oggetto dell'array inserisco la proprietà colore (che dipende dal type)
+//1.2 per ogni oggetto creare una card 
 
 const cardsContainer = document.getElementById("cards-container");
 const filterSelect = document.getElementById("filter-select");
@@ -109,15 +13,34 @@ const filterSelect = document.getElementById("filter-select");
 console.log(cardsContainer);
 console.log(filterSelect);
 
-iconsList.forEach(icon => {
-    icon['color'] = "blue";
-    if (icon.type === "vegetable"){
-      icon['color'] = "yellow";
-    }
-    if (icon.type === "user"){
-      icon['color'] = "purple";
-    }
-   });
+let newIconsList = [...iconsList];
 
-   console.log(iconsList);
+newIconsList.forEach(icon => {
+
+    icon['color'] = "blue";
+
+    if (icon.type === "vegetable") {
+        icon['color'] = "orange";
+    }
+
+    if (icon.type === "user") {
+        icon['color'] = "purple";
+    }
+
+    cardsContainer.innerHTML += `<div class="col">
+                        <div class="card">
+                            <div>
+                                <i class="fas fa-${icon.name}" style="color:${icon.color}"></i>
+                                <h4>${icon.name}</h4>
+                            </div>
+                        </div>
+                    </div>`
+
+});
+
+console.log(newIconsList);
+
+
+
+
 
